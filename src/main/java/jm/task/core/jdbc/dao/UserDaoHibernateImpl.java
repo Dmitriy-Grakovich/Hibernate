f1001237c1f94a6a4e7ae168f4edd96d0fd3068e
Dmitriy-Grakovich <gudvin-gora@mail>
2022-06-17 10:24:37 +0300

    hibernate refactoring1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -20,14 +20,14 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
         Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()){
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             session.createSQLQuery("create table if not exists Users(id bigint primary key auto_increment," +
                     " name varchar(20) not null , lastName varchar(30) not null ," +
                     " age tinyint unsigned not null )").executeUpdate();
             transaction.commit();
-        } catch (Exception e){
-            if(transaction!=null){
+        } catch (Exception e) {
+            if (transaction != null) {
                 transaction.rollback();
             }
         }
@@ -37,12 +37,12 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void dropUsersTable() {
         Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()){
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             session.createSQLQuery("DROP TABLE IF EXISTS Users").executeUpdate();
             transaction.commit();
         } catch (Exception e) {
-            if(transaction!=null){
+            if (transaction != null) {
                 transaction.rollback();
             }
         }
@@ -51,12 +51,12 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void saveUser(String name, String lastName, byte age) {
         Transaction transaction = null;
-        try (Session session = sessionFactory.openSession()){
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
-            session.save(new User(name,lastName,age));
+            session.save(new User(name, lastName, age));
             transaction.commit();
         } catch (Exception e) {
-            if(transaction!=null){
+            if (transaction != null) {
                 transaction.rollback();
             }
         }
